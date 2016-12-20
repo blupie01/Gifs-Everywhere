bdafa0c13fd14637ba22bc307f668a6e365d6e18
blupie01 <jeffrey.chou.89@gmail.com>
2016-12-19 21:27:45 -0500

    finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var initialButtonsArray = [
+var animeButtonsArray = [
   "Evangelion",
   "Naruto",
   "Tokyo Ghoul",
@@ -8,67 +8,99 @@ var initialButtonsArray = [
   "Ergo Proxy"
 ];
 
-function addButton(title) {
-  var button = $("<button id='anime-button' data-title='" + title + "'>" +
-                title + "</button>");
-  $("#buttons-container").append(button);
-}
+//function to display buttons
+function displayButtons() {
+  //clear container to prevent repeat buttons
+  $("#buttons-container").empty();
 
-for (var j = 0; j < initialButtonsArray.length; j++) {
-  addButton(initialButtonsArray[j]);
-}
+  //loop through animeButtonsArray
+  for (var i = 0; i < animeButtonsArray.length; i++) {
+    //create var to hold button tag and add class, data-attribute, and anime title
+    var btn = $("<button>").addClass("anime").attr("data-anime", animeButtonsArray[i]).text(animeButtonsArray[i]);
+    //append button to #buttons-container
+    $("#buttons-container").append(btn);
+  };
+};
 
-$("button").on("click", function() {
-  var animeDataName = $(this).data("title");
-  getAnimeGifs(animeDataName);
-});
+//function to display gifs
+function displayGifs() {
+  //clear container for neatness
+  $("#anime-gifs-container").empty();
 
-$("#add-gif-button").on("click", function(event) {
-  // Preventing the button from trying to submit the form
-  event.preventDefault();
-  // Storing the anime title
-  var anime_Name = $("#search-box").val().trim();
-  addButton(anime_Name);
-  getAnimeGifs(anime_Name);
-});
+  //set var to anime title
+  var anime = $(this).data("anime");
+  //set var for giphy url
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + anime + "&rating=pg-13&api_key=dc6zaTOxFJmzC&limit=20";
 
-function getAnimeGifs(animeTitle) {
-  // Set var to giphy URL with anime title to search for
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animeTitle + 
-                "&rating=pg-13&api_key=dc6zaTOxFJmzC&limit=10";
-
+  //use ajax to get data
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).done(function(response) {
-    //create 
+  }).done(function(response){
     var results = response.data;
 
-    //variable to hold each search result set of gifs
-    var completedGifSet = $("<div id='gif-set'>");
+    for (var k = 0; k < results.length; k++) {
+      //create var to create a <div> to hold each gif/rating pair
+      var gifDiv = $("<div>");
+      //create var to hold rating
+      var rating = results[k].rating;
+      //create var to hold still gif
+      var stillGif = results[k].images.original_still.url;
+      //create var to hold animated gif
+      var animatedGif = results[k].images.original.url;
+      //create var to hold <img> tag with attributes.
+      var animeGif = $("<img>").attr("src", stillGif).attr("data-state", "still").attr(
+                "data-still", stillGif).attr("data-animate", animatedGif).attr("class", "gif");
+      //create var to hold <p> tag for rating
+      var p = $("<p>").text("Rating: " + rating);
 
-    for(var i = 0; i < results.length; i++) {
-      //variable to hold gif and rating
-      var gifDiv = $("<div class='gif-item'>");
-      //variable to get rating from object at index i
-      var rating = results[i].rating;
-      //variable to hold image tag
-      var gifImage = $("<img>");
-      //variable to hold rating of gif
-      var gifRating = $("<p>").text("Rating: " + rating);
+      gifDiv.append(animeGif);
+      gifDiv.append(p);
 
-      //give image source to var gifImage
-      gifImage.attr("src", results[i].images.original.url);
-      //append gif with rating to gifDiv
-      gifDiv.append(gifImage).append(gifRating);
-      //append finished gifDiv with gif and rating to completedGifSet
-      completedGifSet.append(gifDiv);
-
-      //only adds completedGifSet after last gif/rating has been created
-      //sets gifs in order of search and order 
-      if (i == results.length-1) {
-        $("#anime-gifs-container").prepend(completedGifSet);
-      }
-    }
+      //append completed gifDiv to anime-gifs-container
+      $("#anime-gifs-container").append(gifDiv);
+    };
   });
-}
+};
+
+$("#search-box").on("click", function() {
+  $("#search-box").empty();
+})
+
+//jquery to register search click
+$("#search-anime").on("click", function(event) {
+  //prevent default function
+  event.preventDefault();
+  //create var to hold search value
+  var animeSearchInput = $("#search-box").val().trim();
+  //push new search into animeButtonsArray
+  animeButtonsArray.push(animeSearchInput);
+  //call render buttons function
+  displayButtons();
+});
+
+//jquery to stop and start gifs
+$(document).on("click", ".gif", function() {
+  //create var to hold current state the gif is in
+  var state = $(this).attr("data-state");
+  //if state is still do...
+  if (state == "still") {
+    //create var to hold animated gif URL
+    var animate = $(this).attr("data-animate");
+    //change data-state to animate and gif src
+    $(this).attr("data-state", "animate").attr("src", animate);
+  }
+  else
+  {
+    //create var to hold still gif URL
+    var still = $(this).attr("data-still");
+    //change data-state to still and gif src
+    $(this).attr("data-state", "still").attr("src", still);
+  };
+});
+
+//call displayButtons function to put initialArray in #buttons-container
+displayButtons();
+
+//jquery to let buttons clicked run displayGifs function
+$(document).on("click", ".anime", displayGifs);
